616482d2518b7215d36446a91ea136b555495a98
Starle21 <staronova.lenka@gmail.com>
2021-07-22 15:32:47 +0200

    Part 7.5 Anecdotes custom hooks, step2 reset

--- routed-anecdotes/src/components/CreateNew.js
+++ routed-anecdotes/src/components/CreateNew.js
@@ -20,6 +20,14 @@ const CreateNew = (props) => {
     history.push("/");
   };
 
+  const handleReset = (e) => {
+    //
+    e.preventDefault();
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -37,6 +45,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
